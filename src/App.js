1001733e4ab027a49260d86d4b1bfebd4a31c2da
Ana Paula Moreira <aninhapaulinha94@hotmail.com>
2021-01-20 14:03:31 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import { AppContainer } from './components/AppContainer'
+import Header from "./components/Header"
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme } from "@material-ui/core";
+import {ThemeProvider} from "@material-ui/core";
 
-function App() {
-	return (
-        <AppContainer />
+export default function App() {
+
+ 
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#F28C0F'
+        },
+       
+    }
+}); 
+
+return(
+		<div>
+       <CssBaseline/>
+	   <ThemeProvider theme={theme}>
+		<Header/>
+	   </ThemeProvider>
+		</div>
 	)
 }
 
-export default App
+
