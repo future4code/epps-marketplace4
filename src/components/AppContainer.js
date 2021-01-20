2cffcb6211fcb4edd4e1ed90fa2b9a935cf73930
Fernando <fernando.loures@hotmail.com>
2021-01-20 10:56:34 -0300

    apaguei meus dados do App e AppContainer

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import SideBar from './SideBar/SideBar';
-import Filter from './Filter/Filter';
 
 export class AppContainer extends Component {
   render() {
     return (
       <div>
         <p>Pronto para começar!</p>
-        <Filter/>
-        <SideBar/>
       </div>
     )
   }
