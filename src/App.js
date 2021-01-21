6014373ba84621d06657e241c9c1b0616e759ff5
unknown <email-que-voce-usa-no-github>
2021-01-21 18:17:04 -0300

    Início carrinho de compras

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import  AppContainer from './components/AppContainer'
+import ViewBuyProducts from './components/ViewBuyProducts'
+
 
 export default class App extends React.Component{
 	render(){
 		return(
 			<div>
-				<AppContainer/>
+				{/* <AppContainer/> */}
+				<ViewBuyProducts />
 			</div>
 		)
 	}
