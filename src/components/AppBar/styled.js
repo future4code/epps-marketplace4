985d3f03795129a5e13cc92341c3e22eb5c1d264
Allison Araujo <allisonaraujo94@outlook.com>
2021-01-22 07:00:50 -0300

    Tive que excluir o arquivo Styled.js de novo, sem solução!

--- src/components/AppBar/styled.js
+++
@@ -1,8 +0,0 @@
-// import { Typography } from '@material-ui/core';
-import styled from 'styled-components';
-
-    export const Logo = styled.img`
-        width: 100px;
-    `
-
-export default styled
